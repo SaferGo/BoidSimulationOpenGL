9590c6a6a6e75691d065710c24d0fd4058aee3fc
safergo <sfgomez1998@gmail.com>
2021-10-06 11:03:53 +0300

    structure update

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
-#include <SDL2/SDL.h>
 #include <glad/glad.h>
+//#include <SDL2/SDL.h>
+
 #include <iostream>
 
 int main()
 {
-   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+   if (!gladLoadGL()) {
+      std::cout << "Can't load GLAD!" << std::endl;
+      return -1;
+   }
+   std::cout << "OpenGL version loaded: "
+             << GLVersion.major
+             << "."
+             << GLVersion.minor
+             << std::endl;
+
+   /*if (SDL_Init(SDL_INIT_VIDEO) < 0) {
       std::cout << "Failed to initialize the SDL2 library(for video)\n";
       std::cout << "SDL2 Error: " << SDL_GetError() << "\n";
       return -1;
@@ -15,7 +26,7 @@ int main()
                                           SDL_WINDOWPOS_CENTERED,
                                           680,
                                           480,
-                                          0);
+                                          SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 
    if (!window) {
       std::cout << "Failed to create window\n";
@@ -23,15 +34,19 @@ int main()
       return -1;
    }
 
-   SDL_Surface* window_surface = SDL_GetWindowSurface(window);
+   SDL_GLContext glContext = SDL_GL_CreateContext(window);
 
-   if (!window_surface) {
-      std::cout << "Failed to get the surface from the windows\n";
-      std::cout << "SDL2 Error: " << SDL_GetError() << "\n";
+
+   if (!gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress)) {
+      std::cout << "Failed to initialize the OpenGL context." << std::endl;
       return -1;
    }
 
-   SDL_UpdateWindowSurface(window);
-   SDL_Delay(5000);
+   std::cout << "OpenGL version loaded: "
+             << GLVersion.major
+             << "."
+             << GLVersion.minor
+             << std::endl;
+   //glClearColor(0.0f, 1.0f, 0.0f, 0.0f);*/
 	return 0;
 }
