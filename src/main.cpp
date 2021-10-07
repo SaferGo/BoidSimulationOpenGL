90c41f0dde26bcb1c3ac4f98bfc770fd7bd48e72
safergo <sfgomez1998@gmail.com>
2021-10-07 12:33:49 +0300

    structure update

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,96 @@
 #include <glad/glad.h>
-//#include <SDL2/SDL.h>
+#include <SDL2/SDL.h>
 
 #include <iostream>
+#include <exception>
+#include <string.h>
 
-int main()
+void initGLAD()
 {
-   if (!gladLoadGL()) {
-      std::cout << "Can't load GLAD!" << std::endl;
+   // Here we initialize GLAD and loads all the GL function pointers.
+   if (!gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress))
+      throw std::runtime_error("Failed to initialize GLAD.");
+}
+
+void initSDL()
+{
+   if (SDL_Init(SDL_INIT_VIDEO) < 0)
+      throw std::runtime_error(
+               strcat(
+                  (char*)"Failed to initialize the SDL2 library.\nError: ",
+                  SDL_GetError()
+               )
+      );
+}
+
+void initWindow(SDL_Window*& window)
+{
+   window = SDL_CreateWindow("Boid Simulation",
+                             SDL_WINDOWPOS_CENTERED,
+                             SDL_WINDOWPOS_CENTERED,
+                             680,
+                             480,
+                             SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+
+   if (!window)
+      throw std::runtime_error(
+               strcat(
+                  (char*)"Failed to create SDL2 window.\nError: ",
+                  SDL_GetError()
+               )
+      );
+}
+
+void initRenderer(SDL_Renderer*& renderer, SDL_Window*& window)
+{
+   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+   if (!renderer)
+      throw std::runtime_error(
+               strcat(
+                  (char*)"Failed to create SDL2 renderer.\nError: ",
+                  SDL_GetError()
+               )
+      );
+}
+
+int main() 
+{
+
+   SDL_Window* window;
+   SDL_Renderer* renderer;
+
+   try {
+      initSDL();
+      initWindow(window);
+      initRenderer(renderer, window);
+      initGLAD();
+   } catch (const std::exception& e) {
+      std::cout << e.what() << std::endl;
+
       return -1;
    }
-   std::cout << "OpenGL version loaded: "
-             << GLVersion.major
-             << "."
-             << GLVersion.minor
-             << std::endl;
-
-   /*if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-      std::cout << "Failed to initialize the SDL2 library(for video)\n";
-      std::cout << "SDL2 Error: " << SDL_GetError() << "\n";
-      return -1;
-   }
-
-   SDL_Window* window = SDL_CreateWindow("Boid Simulation",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          680,
-                                          480,
-                                          SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
-
-   if (!window) {
-      std::cout << "Failed to create window\n";
-      std::cout << "SDL2 Error: " << SDL_GetError() << "\n";
-      return -1;
-   }
-
+   
    SDL_GLContext glContext = SDL_GL_CreateContext(window);
 
+   SDL_Event event;
 
-   if (!gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress)) {
-      std::cout << "Failed to initialize the OpenGL context." << std::endl;
-      return -1;
+   bool running = true;
+
+   while (running) {
+      /*if (SDL_PollEvent(&event) > 0)
+         std::cout << "Input Detected!\n";
+         */
+
+      glClearColor(1, 0, 0, 1);
+
+      // Updates a window with OpenGL rendering
+      SDL_GL_SwapWindow(window);
    }
 
-   std::cout << "OpenGL version loaded: "
-             << GLVersion.major
-             << "."
-             << GLVersion.minor
-             << std::endl;
-   //glClearColor(0.0f, 1.0f, 0.0f, 0.0f);*/
+   SDL_GL_DeleteContext(glContext);
+   SDL_DestroyRenderer(renderer);
+   SDL_DestroyWindow(window);
+
 	return 0;
 }
